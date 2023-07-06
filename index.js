8a24ca160cdd83e4bcfde4ee7d940a2817d997ed
Sanjay <sanjayacharya992@.com>
2023-07-06 19:16:10 +0530

    c

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const Post = require("./model/PostSchema");
 const app = express();
 // https://sanjay-blog-app.vercel.app
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://sanjay-blog-app.vercel.app",
     credentials:true,
 }))
 
